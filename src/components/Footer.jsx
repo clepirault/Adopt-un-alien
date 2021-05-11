7d47ef114a0b5a3862384d77101c2101dbe3c9cf
Ninon Maraval <maravalninon@gmail.com>
2021-05-11 23:29:58 +0200

    router qui marche enfinnnn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,19 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import './Footer.css';
-import go from '../assets/go.svg'
-import Ma_recherche from '../assets/Ma_recherche.svg'
-import Mon_compte from '../assets/Mon_compte.svg'
-import Compte from './Compte'
-import Home from './Home'
-import ProfilList from './ProfilList'
+import go from '../assets/go.svg';
+import Ma_recherche from '../assets/Ma_recherche.svg';
+import Mon_compte from '../assets/Mon_compte.svg';
+
 
 function Footer(){
   
   return(
     <div className="Footer">
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/compte">
-              <Compte />
-            </Route>
-            <Route path="/pecho">
-              <ProfilList />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
+      <div class="container">
+          <div><img src={Ma_recherche} alt="recherche"/><p>Ma recherche</p></div>
+          <div><img src={Mon_compte} alt="compte"/><p>Mon compte</p></div>
+          <div><img src={go} alt="go"/><p>Go pour pécho</p></div>
         </div>
-               
-        <div class="container">
-        <Link to="/"><div><img src={Ma_recherche} alt="recherche"/><p>Ma recherche</p></div></Link>
-        <Link to="/compte"><div><img src={Mon_compte} alt="compte"/><p>Mon compte</p></div></Link>
-        <Link to="/pecho"><div><img src={go} alt="go"/><p>Go pour pécho</p></div></Link>
-        </div>
-      </Router>  
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -57,6 +57,7 @@ function Home() {
         </div>
         <button type='button'>Go pour pécho !</button>
       </form>
+      
     </div>
   );
 }
