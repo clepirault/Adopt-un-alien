83732ed367b342d6578e0fef079a17d27482a7a7
Clemence Pirault <clepirault@gmail.com>
2021-05-12 11:04:27 +0200

    APP IS FINISHH !

--- src/components/AlienList.jsx
+++ src/components/AlienList.jsx
@@ -14,7 +14,7 @@ const profiles = [
     phrase:
       'Je sais comme personne pétrifier et tétaniser les gens avec mon gros duvet. Une esthéticienne fera mon affaire.',
     planete: 'Monstropolis',
-    hobbies: "TERREUR d'élite",
+    hobbies: "TERREUR d'élite"
   },
   {
     id: 2,
@@ -27,7 +27,7 @@ const profiles = [
     phrase: "Epouvanter, c'est mon métier!",
     planete: "Neiges de l'Himalaya",
     hobbies:
-      'Regler les crises d’énergie, la baisse des bénéfices et retrouver une petite fille humaine en cavale',
+      'Regler les crises d’énergie, la baisse des bénéfices et retrouver une petite fille humaine en cavale'
   },
   {
     id: 3,
@@ -40,7 +40,7 @@ const profiles = [
     phrase: 'Parle très vite mais agit peu',
     planete: 'Décapotable',
     hobbies:
-      'Dirige tous les matins ma séance d’entraînement : pompes, abdominaux, poses effrayantes et rugissements sont au programme. ',
+      'Dirige tous les matins ma séance d’entraînement : pompes, abdominaux, poses effrayantes et rugissements sont au programme. '
   },
   {
     id: 4,
@@ -53,10 +53,42 @@ const profiles = [
     phrase:
       "A cheval sur les détails et les principes, j'ai une vivacité d'esprit surprenante pour une limace",
     planete: 'Epouvanteuse',
-    hobbies: 'lire des journaux à scandale pendant mon heure de déjeuner',
+    hobbies: 'lire des journaux à scandale pendant mon heure de déjeuner'
   },
   {
     id: 5,
+    species: 'Humain',
+    name: 'Nabila Benatia',
+    image: 'https://mediamass.net/jdd/public/documents/celebrities/5493.jpg',
+    bonnitude: "Canon de l'espace",
+    phrase: 'Être acceptée comme je suis',
+    planete: 'Terre',
+    hobbies: 'Les produits capillaires'
+  },
+  {
+    id: 20,
+    species: 'Humain',
+    name: 'Jacky Propre',
+    image:
+      'https://pm1.narvii.com/7150/9a2fdbafc54576754348c2de91f08d11d05db8ear5-681-382_00.jpg',
+    bonnitude: "Canon de l'espace",
+    phrase: "Là où la propreté fait défaut, l'amour ne reste pas en vie",
+    planete: 'Terre',
+    hobbies: "Collection d'éponge"
+  },
+  {
+    id: 17,
+    species: 'Humain',
+    name: 'James Franco',
+    image:
+      'https://i1.wp.com/www.starmag.com/wp-content/uploads/2018/06/james-franco-750x400.jpg?resize=1200%2C675&ssl=1',
+    bonnitude: "Canon de l'espace",
+    phrase: 'Chuis chaud comme la braise',
+    planete: 'Terre',
+    hobbies: 'Attraper des poux'
+  },
+  {
+    id: 18,
     species: 'Humain',
     name: 'Marie Gibbs',
     age: 4,
@@ -66,7 +98,7 @@ const profiles = [
     phrase: "J'ai peur de rien et je suis insconciente",
     planete: 'Minou',
     hobbies:
-      'de voir à quel point l’énergie que je produit par mon rire peut être puissante.',
+      'de voir à quel point l’énergie que je produit par mon rire peut être puissante.'
   },
   {
     id: 6,
@@ -78,7 +110,7 @@ const profiles = [
     bonnitude: "Canon de l'espace",
     phrase: 'Douce et ensorcelante.',
     planete: 'Minou',
-    hobbies: 'Le luxe et les poils',
+    hobbies: 'Le luxe et les poils'
   },
   {
     id: 7,
@@ -91,7 +123,7 @@ const profiles = [
     phrase:
       "J'aime préparer en cachette un plan diabolique pour arriver à mes fins.",
     planete: 'Machine infernale',
-    hobbies: 'Inventer un système pour arracher des hurlements aux enfants.',
+    hobbies: 'Inventer un système pour arracher des hurlements aux enfants.'
   },
   {
     id: 8,
@@ -104,7 +136,7 @@ const profiles = [
     phrase:
       "J'aime sortir ma tenue d'urgence pour me protéger des contaminations",
     planete: 'Chauve souris',
-    hobbies: 'Arracher des cheveux dans une fourrure de monstre',
+    hobbies: 'Arracher des cheveux dans une fourrure de monstre'
   },
   {
     id: 9,
@@ -116,7 +148,7 @@ const profiles = [
     bonnitude: "Canon de l'espace",
     phrase: 'Si je me coupe les ongles des pieds je fais du 42 sinon du 56',
     planete: 'Epouvanteuse',
-    hobbies: 'Aiguiser les griffes,brosser les crocs et limer les cornes.',
+    hobbies: 'Aiguiser les griffes,brosser les crocs et limer les cornes.'
   },
   {
     id: 10,
@@ -130,7 +162,7 @@ const profiles = [
       'Si tu es petit rond et marron et que tu sens pas bon, tu es fais pour moi',
     planete: '',
     hobbies:
-      'Les grandes balades sur la plage et construire en secret des machines à aspirer des crottes.',
+      'Les grandes balades sur la plage et construire en secret des machines à aspirer des crottes.'
   },
   {
     id: 11,
@@ -141,7 +173,7 @@ const profiles = [
     bonnitude: 'Débris de la galaxie',
     phrase: 'Moche et sans ami – sauras-tu me complimenter ? ',
     planete: 'ZigmaB',
-    hobbies: 'Regarder la Tv et manger des sandwichs',
+    hobbies: 'Regarder la Tv et manger des sandwichs'
   },
   {
     id: 12,
@@ -153,7 +185,7 @@ const profiles = [
     phrase:
       'Si un jour ton cœur ne bat plus, je te donnerai le mien, car sans toi, il me sert à rien.',
     planete: 'ZigmaB',
-    hobbies: 'Raconter des mythos',
+    hobbies: 'Raconter des mythos'
   },
   {
     id: 13,
@@ -165,7 +197,7 @@ const profiles = [
     phrase:
       'Si à chaque fois que je pensais à toi, il poussait une fleur, alors la Terre serait un immense jardin de roses.',
     planete: 'ZigmaB',
-    hobbies: "lire un livre, boire de l'eau",
+    hobbies: "lire un livre, boire de l'eau"
   },
   {
     id: 14,
@@ -177,7 +209,7 @@ const profiles = [
     phrase:
       'Je suis nouveau en ville, pourrais-tu m’indiquer le chemin pour aller à ton appartement ?',
     planete: 'ZigmaB',
-    hobbies: 'Manger, dormir et inversement',
+    hobbies: 'Manger, dormir et inversement'
   },
   {
     id: 15,
@@ -188,7 +220,7 @@ const profiles = [
     bonnitude: "Canon de l'espace",
     phrase: "Besoin de quelqu'un pour monter un meuble Ikea",
     planete: 'Terre',
-    hobbies: 'Boire un verre en terasse avec vu sur Neptune',
+    hobbies: 'Boire un verre en terasse avec vu sur Neptune'
   },
   {
     id: 16,
@@ -199,34 +231,11 @@ const profiles = [
     bonnitude: 'Débris de la galaxie',
     phrase: 'Besoin de tendresse dans ce monde de brutes',
     planete: 'Terre',
-    hobbies: 'Tuer',
+    hobbies: 'Tuer'
   },
-  {
-    id: 17,
-    species: 'humain',
-    name: 'James Franco',
-    image:
-      'https://i1.wp.com/www.starmag.com/wp-content/uploads/2018/06/james-franco-750x400.jpg?resize=1200%2C675&ssl=1',
-    bonnitude: "Canon de l'espace",
-    phrase: 'Chuis chaud comme la braise',
-    planete: 'Terre',
-    hobbies: 'Attraper des poux',
-  },
-
-  {
-    id: 18,
-    species: 'humain',
-    name: 'Nabila Benatia',
-    image: 'https://mediamass.net/jdd/public/documents/celebrities/5493.jpg',
-    bonnitude: "Canon de l'espace",
-    phrase: 'Être acceptée comme je suis',
-    planete: 'Terre',
-    hobbies: 'Les produits capillaires',
-  },
-
   {
     id: 19,
-    species: 'humain',
+    species: 'Humain',
     name: 'Eve angeli',
     image:
       'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2Fb4d37919-b2cc-405c-b4be-34720f4e09cc.2Ejpeg/920x518/quality/80/eve-angeli.jpg',
@@ -234,24 +243,11 @@ const profiles = [
     phrase:
       "On dit que l'amour se trouve à chaque coin de rue. Je dois vivre sur un rond point",
     planete: 'Terre',
-    hobbies: 'Physique Quantique',
+    hobbies: 'Physique Quantique'
   },
-
-  {
-    id: 20,
-    species: 'humain',
-    name: 'Monsieur Propre',
-    image:
-      'https://pm1.narvii.com/7150/9a2fdbafc54576754348c2de91f08d11d05db8ear5-681-382_00.jpg',
-    bonnitude: "Canon de l'espace",
-    phrase: "Là où la propreté fait défaut, l'amour ne reste pas en vie",
-    planete: 'Terre',
-    hobbies: "Collection d'éponge",
-  },
-
   {
     id: 21,
-    species: 'humain',
+    species: 'Humain',
     name: 'Evelyne Dheliat',
     image:
       'https://photos.tf1.fr/700/933/evelyne-dheliat-portrait2-f71409-0@1x.jpg',
@@ -259,13 +255,13 @@ const profiles = [
     phrase:
       " L'amour, c'est le soleil après la pluie, et la luxure, c'est l'orage après le soleil.",
     planete: 'Terre',
-    hobbies: 'Danser sous la pluie',
+    hobbies: 'Danser sous la pluie'
   },
 ];
+
 let allMatches = [];
 function AlienList() {
   const [position, setPosition] = useState(0);
-  /* const [match, setMatch] = useState(''); */
 
   const profileSelection = profiles.filter(
     (element) =>
