9e97759ac7e31cf7bc9a23e21656bcb5bc194021
Ninon Maraval <maravalninon@gmail.com>
2021-05-12 09:38:10 +0200

    local storage ne crash plus

--- src/components/Compte.jsx
+++ src/components/Compte.jsx
@@ -3,25 +3,29 @@ import MatchItem from './MatchItem';
 import profil from './profil.jpg';
 
 function Compte() {
-  const allMatches = JSON.parse(localStorage.getItem('match'));
+  const allMatches = JSON.parse(localStorage.getItem('match')) || [];
   console.log(allMatches);
   return (
     <div className='compte'>
-     <div className="profile-header">
+      <div className='profile-header'>
         <h3>Profil de:</h3>
         <h1>La Denrée</h1>
       </div>
-      <div className="user">
-        <img className="matchesAvatar" src={profil} alt='userAvatar' />
-        <p><strong>Ma philosophie :</strong> <br /> Bloubloubloubloublou</p>
-        <p><strong>Mes hobbies :</strong> j'aime manger la soupe aux choux !</p>
+      <div className='user'>
+        <img className='matchesAvatar' src={profil} alt='userAvatar' />
+        <p>
+          <strong>Ma philosophie :</strong> <br /> Bloubloubloubloublou
+        </p>
+        <p>
+          <strong>Mes hobbies :</strong> j'aime manger la soupe aux choux !
+        </p>
       </div>
-      <div className="matches-box">
-      <h1>Mes matchs ❤️</h1>
-      <MatchItem {...allMatches[0]} />
-      <MatchItem {...allMatches[1]} />
-      <MatchItem {...allMatches[2]} />
-    </div>
+      <div className='matches-box'>
+        <h1>Mes matchs ❤️</h1>
+        {allMatches.slice(0, 3).map((match) => {
+          return { ...match };
+        })}
+      </div>
     </div>
   );
 }
